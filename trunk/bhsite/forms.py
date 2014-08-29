99ab55f6efff46f81e8254359d6e81ac9d709d63
bh <kanom@gmx.us>
2014-08-29 17:23:33 +0000

    Content improvements.

--- trunk/bhsite/forms.py
+++ trunk/bhsite/forms.py
@@ -1,23 +1,22 @@
 from django import forms
 
 class HomeForm(forms.Form):
-    expiry = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M'])
-    amount 	= forms.FloatField()
-    rate 	= forms.FloatField()
-    address = forms.CharField(max_length=35, required=False)
-    select_direction = forms.ChoiceField(choices=[(0, 'Decrease'), (1, 'Increase')])
+    expiry              = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M'])
+    amount 	            = forms.FloatField()
+    rate 	            = forms.FloatField()
+    address             = forms.CharField(max_length=35, required=False)
+    select_direction    = forms.ChoiceField(choices=[(0, 'Decrease'), (1, 'Increase')])
 
 class SubmitForm(forms.Form):
-    expiry 	= forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M'])
-    amount 	= forms.FloatField()
-    rate 	= forms.FloatField()
-    address = forms.CharField(max_length=35)
-    bh_address = forms.CharField(max_length=35)
-    select_direction = forms.ChoiceField(choices=[(0, 'Decrease'), (1, 'Increase')])
+    expiry 	            = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M'])
+    amount 	            = forms.FloatField()
+    rate 	            = forms.FloatField()
+    address             = forms.CharField(max_length=35)
+    bh_address          = forms.CharField(max_length=35)
+    select_direction    = forms.ChoiceField(choices=[(0, 'Decrease'), (1, 'Increase')])
 
 class BitcoinRecipient(forms.Form):
-    bitcoin_recipient = forms.CharField(max_length=100)
-
+    bitcoin_recipient   = forms.CharField(max_length=100)
 
 #~ class RegisterForm(BitcoinRecipient):
     #~ email = forms.EmailField()
